1db22361016a1b2e7deaf9d274179d00bd08e109
Alexander Barge <alexander.barge@gmail.com>
2019-11-11 10:17:34 +0100

    add try catch

--- src/core.js
+++ src/core.js
@@ -10,31 +10,46 @@ export function getTracker() {
 }
 
 export function trackPageview() {
-  getTracker().push({ notify: 'event', name: 'view_content' });
-  getTracker().push({ notify: 'event', name: 'page_view' });
+  try {
+    getTracker().push({ notify: 'event', name: 'view_content' });
+    getTracker().push({ notify: 'event', name: 'page_view' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export function trackEvent(eventAction = '') {
-  getTracker().push({ notify: 'event', name: eventAction });
+  try {
+    getTracker().push({ notify: 'event', name: eventAction });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export function initialize(newTrackerId) {
-  /* eslint-disable */
-  !(function(t, f, a, x) {
-    if (!document.getElementById(x)) {
-      t.async = 1;
-      t.src = a;
-      t.id = x;
-      f.parentNode.insertBefore(t, f);
-    }
-  })(
-    document.createElement('script'),
-    document.getElementsByTagName('script')[0],
-    `//cdn.taboola.com/libtrc/unip/${newTrackerId}/tfa.js`,
-    'tb_tfa_script',
-  );
-  /* eslint-enable */
-  trackPageview();
+  try {
+    /* eslint-disable */
+    !(function(t, f, a, x) {
+      if (!document.getElementById(x)) {
+        t.async = 1;
+        t.src = a;
+        t.id = x;
+        f.parentNode.insertBefore(t, f);
+      }
+    })(
+      document.createElement('script'),
+      document.getElementsByTagName('script')[0],
+      `//cdn.taboola.com/libtrc/unip/${newTrackerId}/tfa.js`,
+      'tb_tfa_script',
+    );
+    /* eslint-enable */
+    trackPageview();
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.warn(e);
+  }
 }
 
 export default {
