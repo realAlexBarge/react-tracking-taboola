d7f21d1da38a2c238f5caf01f73531feeeac5255
Marcon <marcon@mh4sh.dev>
2021-09-17 16:10:49 -0300

    chore ✨: Add typescript files

---
+++ ts/index.d.ts
@@ -0,0 +1,8 @@
+import { initialize, getTracker, trackPageview, trackEvent } from './core';
+declare const _default: {
+    initialize: typeof initialize;
+    getTracker: typeof getTracker;
+    trackPageview: typeof trackPageview;
+    trackEvent: typeof trackEvent;
+};
+export default _default;
